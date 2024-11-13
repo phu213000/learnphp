ba2b359c539d329afe601a27cd68a9757084d030
Hồ Trường Minh Phú <117896798+phu213000@users.noreply.github.com>
2024-11-13 16:16:12 +0700

    toi global-request

--- php/index.php
+++ php/index.php
@@ -1,29 +1,97 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
 <body>
 
 <?php
 
-$txt1 = "Learn PHP";
-$txt2 = "W3Schools.com";
+// $txt1 = "Learn PHP";
+// $txt2 = "W3Schools.com";
 
-echo '<h2>Hello world</h2>';
-echo "I am $txt1 from $txt2 <br>";
+// echo '<h2>Hello world</h2>';
+// echo "I am $txt1 from $txt2 <br>";
 
-$x = 5;
-var_dump($x);
+// $x = 5;
+// var_dump($x);
 
-echo "Hello my friends <br>";
-echo ("Toi ten Phu, la nguoi lap trinh vien luoi bieng , ngu dot nhung toi rat thich hoc PHP<br>");
+// echo "Hello my friends <br>";
+// echo ("Toi ten Phu, la nguoi lap trinh vien luoi bieng , ngu dot nhung toi rat thich hoc PHP<br>");
 
 
-$x = "John";
-echo 'Hello '.$x.'<br>';
+// $x = "John";
+// echo 'Hello '.$x.'<br>';
 
-echo strlen($x);
-echo str_word_count("Hello world!");
-echo strpos("Hello world!", "world");
+// echo strlen($x);
+// echo str_word_count("Hello world!");
+// echo strpos("Hello world!", "world");
 
+// $y = "Hello , this is my boy friend Phu dep trai<br>";
+// echo strtoupper($y);
+
+// $z = "Hello world ";
+// echo strtolower($z);
+
+// $t = "Hello world!";
+// echo str_replace("world", "Dolly", $t);
+
+// $l = "Phu dep trai";
+// echo trim($l);
+
+// $cars = array("Volovo", "BMW", "Toyota");
+// unset($cars[1]);
+
+// $cars= array("Volvo", "BMW", "Toyota");
+// array_splice($cars, 1, 1);
+// print_r($cars);
+
+// $car1 = array("Volvo", 100, 96);
+// unset($car1[0]);
+// print("<pre>");
+// print_r($car1);
+
+// $cars = array (
+//   array("Volvo",22,18),
+//   array("BMW",15,13),
+//   array("Saab",5,2),
+//   array("Land Rover",17,15)
+// );
+// for ($row = 0; $row < 4; $row++) {
+//   echo "<p><b>Row number $row</b></p>";
+//   echo "<ul>";
+//   for ($col = 0; $col < 3; $col++) {
+//     echo "<li>".$cars[$row][$col]."</li>";
+//   }
+//   echo "</ul>";
+// }
+
+// echo $cars[0][0]. "In stock :".$cars[0][1].", sold :".$cars[0][2].".<br>";
+// echo $cars[1][0]. "In stock :".$cars[1][1].", sold :".$cars[1][2].".<br>";
+// echo $cars[2][0]. "In stock :".$cars[2][1].", sold :".$cars[2][2].".<br>";
+// echo $cars[3][0]. "In stock :".$cars[3][1].", sold :".$cars[3][2].".<br>";
+
+
+?>
+
+</body>
+</html> -->
+<!DOCTYPE html>
+<html>
+<body>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  Name: <input type="text" name="fname">
+  <input type="submit">
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // collect value of input field
+    $name = htmlspecialchars($_REQUEST['fname']);
+    if (empty($name)) {
+        echo "Name is empty";
+    } else {
+        echo $name;
+    }
+}
 ?>
 
 </body>
