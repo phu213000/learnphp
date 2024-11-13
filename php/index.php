bbafe8a0f6ee6ac71bf0d4dea21aec0b0bebfe46
Hồ Trường Minh Phú <117896798+phu213000@users.noreply.github.com>
2024-11-13 14:40:19 +0700

    upload lan dau

---
+++ php/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$txt1 = "Learn PHP";
+$txt2 = "W3Schools.com";
+
+echo '<h2>Hello world</h2>';
+echo "I am $txt1 from $txt2 <br>";
+
+$x = 5;
+var_dump($x);
+
+echo "Hello my friends <br>";
+echo ("Toi ten Phu, la nguoi lap trinh vien luoi bieng , ngu dot nhung toi rat thich hoc PHP<br>");
+
+
+$x = "John";
+echo 'Hello '.$x.'<br>';
+
+echo strlen($x);
+
+?>
+
+</body>
+</html>
