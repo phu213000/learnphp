5ba18af8d7cc36748a548e44e76e3f49c51b4185
Hồ Trường Minh Phú <117896798+phu213000@users.noreply.github.com>
2024-11-15 12:04:58 +0700

    them file validation.php

---
+++ php/validation.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Example Validation Form</title>
+</head>
+<body>
+  <form method="post" action="validation.php">
+    Name: <input type="text" name="username" placeholder="Input your username ">
+    Email: <input type="email" name="user_email" placeholder="Input your gmail ">
+    Password: <input type="password" name="user_pass" placeholder="Input your password ">
+    <button type="submit">Register</button>
+  </form>
+</body>
+</html>
---
+++ php/welcome.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Form login</title>
+</head>
+<body>
+  <form action="welcome.php" method="post">
+    Name: <input type="text" placeholder="Phu Ho" >
+    Password : <input type="password" name="password" id="password">
+    <button type="submit" name="login">login</button>
+  </form>
+</body>
+</html>
